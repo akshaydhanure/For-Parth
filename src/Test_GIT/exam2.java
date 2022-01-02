829240c02e6ddee4a51388e6af64601c22a55883
Ravi Dhanure <Ravi Dhanure@DESKTOP-2FC8JKB>
2022-01-02 11:40:21 +0530

    added new please chk

---
+++ src/Test_GIT/exam2.java
@@ -0,0 +1,15 @@
+package Test_GIT;
+
+public class exam2 
+{
+public static void main(String[] args) 
+{
+char grade []= {'A','B','C'};
+
+for (int i=0;i<=grade.length-1;i++)
+{
+	System.out.println(grade[i]);
+}
+	
+}
+}
